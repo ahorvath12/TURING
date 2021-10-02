48ad295058f013faf786720b21bae0f680a58a14
ahorvath12 <andreahorvath1998@gmail.com>
2021-10-01 20:01:56 -0700

    dialogue

---
+++ TURING/Assets/Scripts/Dialogue/DialogueViewer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static DialogueObject;
+using UnityEngine.Events;
+using System;
+using System.Runtime.InteropServices;
+
+public class DialogueViewer : MonoBehaviour
+{
+    [SerializeField] Transform parentOfResponses;
+    [SerializeField] Button prefab_btnResponse;
+    [SerializeField] Text txtMessage;
+    DialogueController controller;
+
+    [DllImport("__Internal")]
+    private static extern void openPage(string url);
+
+    private void Start()
+    {
+        controller = GetComponent<DialogueController>();
+        controller.onEnteredNode += OnNodeEntered;
+        controller.InitializeDialogue();
+
+        // Start dialogue
+        var curNode = controller.GetCurrentNode();
+    }
+
+    public static void KillAllChildren(UnityEngine.Transform parent)
+    {
+        UnityEngine.Assertions.Assert.IsNotNull(parent);
+        for (int childIndex = parent.childCount - 1; childIndex >= 0; childIndex--)
+        {
+            UnityEngine.Object.Destroy(parent.GetChild(childIndex).gameObject);
+        }
+    }
+
+    private void OnNodeSelected(int indexChosen)
+    {
+        Debug.Log("Chose: " + indexChosen);
+        controller.ChooseResponse(indexChosen);
+    }
+
+    private void OnNodeEntered(Node newNode)
+    {
+        txtMessage.text = newNode.text;
+        KillAllChildren(parentOfResponses);
+
+        //UnityAction typeResponsesAfterMessage = delegate {
+            for (int i = newNode.responses.Count - 1; i >= 0; i--)
+            {
+                int currentChoiceIndex = i;
+                var response = newNode.responses[i];
+                var responceButton = Instantiate(prefab_btnResponse, parentOfResponses);
+                responceButton.GetComponentInChildren<Text>().text = response.displayText;
+            Debug.Log(response.displayText);
+                responceButton.onClick.AddListener(delegate { OnNodeSelected(currentChoiceIndex); });
+            }
+        //};
+    }
+
+}
--- TURING/Assets/Scripts/GameManager.cs
+++ TURING/Assets/Scripts/GameManager.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text timeText;
+    public GameObject mainUI;
+    public GameObject gameOverUI;
+
+    private float timeRemaining = 60;
+
     void Start()
     {
         Screen.SetResolution(561, 483, false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            timeText.text = Mathf.FloorToInt(timeRemaining % 60).ToString();
+        }
+        else
+        {
+            //end
+            timeText.text = "0";
+            gameOverUI.SetActive(true);
+            mainUI.GetComponent<CanvasGroup>().interactable = false;
+        }
     }
 }
